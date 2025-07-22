78df914e4b584b6346f8a58fa195e6998c1abc2b
Kaushk-143 <131331800+Kaushk-143@users.noreply.github.com>
2025-07-23 02:02:38 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     extend: {
       screens: {
         'xs': '475px',
+        '2xs': '375px',
+        '3xs': '320px',
+      },
+      spacing: {
+        '18': '4.5rem',
+        '88': '22rem',
+        '128': '32rem',
+      },
+      fontSize: {
+        '2xs': ['0.625rem', { lineHeight: '0.75rem' }],
       },
     },
   },
